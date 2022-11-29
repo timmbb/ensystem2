e9bb66a5065ee3da7e57df635ab345f2beacf722
timmbb <txy1112021@126.com>
2022-11-29 20:12:06 +0800

    Initial commit

---
+++ src/main/java/com/example/ensystem2/Ensystem2Application.java
@@ -0,0 +1,13 @@
+package com.example.ensystem2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ensystem2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ensystem2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ensystem2/Ensystem2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ensystem2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ensystem2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
