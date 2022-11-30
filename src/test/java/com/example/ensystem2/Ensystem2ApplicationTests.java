4928e156f7d33852ca07466ab80e6f13b7d8fe74
timmbb <txy1112021@126.com>
2022-11-30 12:26:51 +0800

    1 add

--- src/test/java/com/example/ensystem2/Ensystem2ApplicationTests.java
+++ src/test/java/com/example/ensystem2/Ensystem2ApplicationTests.java
@@ -1,13 +1,23 @@
 package com.example.ensystem2;
 
+import com.example.ensystem2.bean.user;
+import com.example.ensystem2.service.userservice;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.annotation.Resource;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class Ensystem2ApplicationTests {
-
+    @Resource
+    userservice userService;
     @Test
     void contextLoads() {
+        user userbean = userService.logIn("Diluc","diluc03");
+        System.out.println("user id is: ");
+        System.out.println(userbean.getId());
     }
-
 }
